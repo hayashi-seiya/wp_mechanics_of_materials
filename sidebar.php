a993f610ae3a0895973972d567dcd8042173bf66
hayashi-seiya <sof8584@gmail.com>
2016-10-29 19:06:38 +0900

    目次を動的に出力するための準備

--- sidebar.php
+++ sidebar.php
@@ -13,6 +13,10 @@
   <ul>
     <li>
       <a href="#01">応力とは</a>
+      <!--
+      TODO <a>タグのあとのテキストをページごとに動的に出力したい
+      参照URL: http://media.tech-salon.com/php-%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%96%E5%BE%97-pregmatch/
+      -->
     </li>
   </ul>
 </div>
