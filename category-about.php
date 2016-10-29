c05967d60f0f746bbe18903b72693ac6fc483e18
hayashi-seiya <sof8584@gmail.com>
2016-10-29 19:03:16 +0900

    材料力学を学ぶとは？の途中

--- category-about.php
+++ category-about.php
@@ -9,6 +9,21 @@
     <div class="col-md-9">
       <article class=<?php post_class(); ?>>
 
+        <h2 class="h2">材料力学を学ぶとは？</h2>
+
+        <h3 class="h3" id="01">材料力学とは</h3>
+        <div class="introContents">
+          <p>
+            機械や構造物に作用する種々の外力に対し、各部材の強さ、合成および安定性がどのようになっているか、
+            理論と実験の両面から明らかにしておく必要がある。
+            このような学問を<span class="strong">材料力学 (mechanics of materials)</span>という。
+          </P>
+          <p>
+            機械や構造物を構成している部材は一見すると複雑な形状をしているようであるが、
+            これらは単純な構造要素に分解できるものが多く、通常は棒、平板、あるいは曲面版などに分類できる。
+          </p>
+
+
       </article>
     </div>
 
