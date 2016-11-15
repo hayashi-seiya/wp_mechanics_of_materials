b79a6da4616729bb602bf864a758476a02451ffd
hayashi-seiya <sof8584@gmail.com>
2016-11-15 11:00:41 +0900

    目次を出力できそうでできない

--- sidebar.php
+++ sidebar.php
@@ -2,9 +2,7 @@
   <div class="container">
     <div class="row">
       <h2>カテゴリー</h2>
-      <?php
-        wp_nav_menu( $defaults );
-      ?>
+      <?php wp_nav_menu( $defaults ); ?>
     </div>
   </div>
 </nav>
@@ -12,10 +10,15 @@
   <h2>目次</h2>
   <ul>
     <li>
-      <a href="#01">応力とは</a>
+          <?php
+          $indexes = getIndex();
+          foreach($indexes as $index){
+              echo "<a>".$index."</a>";
+          }
+          ?>
       <!--
-      TODO <a>タグのあとのテキストをページごとに動的に出力したい
-      参照URL: http://media.tech-salon.com/php-%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%96%E5%BE%97-pregmatch/
+        TODO <a>タグのあとのテキストをページごとに動的に出力したい
+        参照URL: http://media.tech-salon.com/php-%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%96%E5%BE%97-pregmatch/
       -->
     </li>
   </ul>
